2c190a19ef4ee44eb66ad2b11ac8f21b3e5e5685
Gabriel Perez <gapc87@gmail.com>
2020-05-10 13:46:02 +0200

    vista para crear un endpoint de lectura-escritura que enumere todas las instancias de Snippet

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Snippet
+from .serializers import SnippetSerializer
 
-# Create your views here.
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
