7e5418fc216c0ea1c50323e9b6782ab59520f3eb
Gabriel Perez <gapc87@gmail.com>
2020-05-10 13:46:57 +0200

    vista para crear un endpoint de lectura-escritura-eliminacion para cada Snippet individual

--- snippets/views.py
+++ snippets/views.py
@@ -6,3 +6,8 @@ from .serializers import SnippetSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
