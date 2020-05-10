fbb699d980352cb641b0b3f116fb500bb853f55a
Gabriel Perez <gapc87@gmail.com>
2020-05-10 15:16:54 +0200

    highlighted como un campo de solo lectura

--- snippets/admin.py
+++ snippets/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Snippet
 
-admin.site.register(Snippet)
+
+class SnippetAdmin(admin.ModelAdmin):
+    readonly_fields = ('highlighted',)
+
+
+admin.site.register(Snippet, SnippetAdmin)
